154c033465ef1b697ca5986a8407e8a1625e9290
Anduin Xue <anduin@aiursoft.com>
2024-09-12 10:48:46 +0000

    Add unit tests for Aiursoft.UiStack.Demo
    Created a new test project `Aiursoft.UiStack.Demo.Tests` to include unit tests for the Aiursoft.UiStack.Demo project. Added a basic test that ensures the home page loads successfully. Updated the solution file to include the new test project.

---
+++ tests/Aiursoft.UiStack.Demo.Tests/BasicTests.cs
@@ -0,0 +1,44 @@
+﻿using Aiursoft.CSTools.Tools;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Aiursoft.WebTools.Extends;
+
+namespace Aiursoft.UiStack.Demo.Tests;
+
+[TestClass]
+public class BasicTests
+{
+    private readonly string _endpointUrl;
+    private readonly int _port;
+    private readonly HttpClient _http;
+    private IHost? _server;
+
+    public BasicTests()
+    {
+        _port = Network.GetAvailablePort();
+        _endpointUrl = $"http://localhost:{_port}";
+        _http = new HttpClient();
+    }
+
+    [TestInitialize]
+    public async Task CreateServer()
+    {
+        _server = await AppAsync<Startup>([], port: _port);
+        await _server.StartAsync();
+    }
+
+    [TestCleanup]
+    public async Task CleanServer()
+    {
+        if (_server == null) return;
+        await _server.StopAsync();
+        _server.Dispose();
+    }
+
+    [TestMethod]
+    public async Task TestGetHome()
+    {
+        var response = await _http.GetAsync(_endpointUrl);
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+    }
+}
