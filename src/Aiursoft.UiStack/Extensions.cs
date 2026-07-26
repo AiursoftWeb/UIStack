0e13f9e2c832ac09a0fa229cee480ae5ecd54591
Anduin Xue <anduin@aiursoft.com>
2026-07-26 17:39:27 +0000

    feat: update version to 10.0.22 and enhance frontend build process

--- src/Aiursoft.UiStack/Extensions.cs
+++ src/Aiursoft.UiStack/Extensions.cs
@@ -1,5 +1,8 @@
+using System.Reflection;
 using Aiursoft.UiStack.Layout;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 
 namespace Aiursoft.UiStack;
 
@@ -13,5 +16,23 @@ public static class Extensions
     public static ViewResult UiStackView(this Controller controller, UiStackLayoutViewModel model, string viewName)
     {
         return controller.View(viewName, model);
+    }
+
+    /// <summary>
+    /// Serve UIStack static assets (CSS, JS, fonts, images) from embedded resources.
+    /// Must be called before UseStaticFiles() in Startup.Configure.
+    /// </summary>
+    public static IApplicationBuilder UseUIStack(this IApplicationBuilder app)
+    {
+        var assembly = typeof(Extensions).Assembly;
+        var embeddedProvider = new EmbeddedFileProvider(assembly, $"{assembly.GetName().Name}.wwwroot.dist");
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = embeddedProvider,
+            RequestPath = "/UIStack/dist"
+        });
+
+        return app;
     }
 }
